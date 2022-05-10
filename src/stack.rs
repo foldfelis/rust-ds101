8cd7261e972e5cadd1f625ca3b5936ad4a6b8ff5
JingYu Ning <foldfelis@gmail.com>
2022-05-10 21:42:55 +0800

    Reimplement stack in Vec representation

--- src/stack.rs
+++ src/stack.rs
@@ -1,37 +1,22 @@
 #[derive(Debug)]
-pub struct Stack10 {
-    es: [i64; 10],
-    top: isize
+pub struct Stack<T> {
+    es: Vec<T>
 }
 
-impl Stack10 {
-    pub fn new() -> Stack10 {
-        return Stack10 { es: [0; 10], top: -1 }
+impl<T> Stack<T> {
+    pub fn new() -> Stack<T> {
+        return Stack { es: Vec::new() }
     }
 
-    pub fn len(&self) -> isize {
-        return self.top + 1;
+    pub fn len(&self) -> usize {
+        return self.es.len();
     }
 
-    pub fn push(&mut self, e: i64) {
-        self.increase_top();
-        self.es[self.top as usize] = e;
+    pub fn push(&mut self, e: T) {
+        self.es.push(e);
     }
 
-    pub fn pop(&mut self) -> i64 {
-        let e = self.es[self.top as usize];
-        self.decrease_top();
-
-        return e;
-    }
-
-    fn increase_top(&mut self) {
-        self.top += 1;
-        assert!(self.top <= 10);
-    }
-
-    fn decrease_top(&mut self) {
-        self.top -= 1;
-        assert!(self.top >= -1);
+    pub fn pop(&mut self) -> Option<T> {
+        return self.es.pop();
     }
 }
--- tests/stack.rs
+++ tests/stack.rs
@@ -1,41 +1,38 @@
-use rust_ds101::stack::Stack10;
+use rust_ds101::stack::Stack;
 
 #[test]
-fn stack10_push() {
-    let mut stack10 = Stack10::new();
+fn stack_push() {
+    let mut stack = Stack::new();
 
-    stack10.push(1);
-    assert_eq!(stack10.len(), 1);
+    stack.push(1);
+    assert_eq!(stack.len(), 1);
 }
 
 #[test]
-fn stack10_pop() {
-    let mut stack10 = Stack10::new();
+fn stack_pop() {
+    let mut stack = Stack::new();
 
-    stack10.push(1);
-    let e = stack10.pop();
-    assert_eq!(e, 1);
+    stack.push(1);
+    assert_eq!(stack.pop(), Some(1));
 }
 
 #[test]
-fn stack10_push_10() {
-    let mut stack10 = Stack10::new();
+fn stack_push_10() {
+    let mut stack = Stack::new();
 
     for i in 1..=10 {
-        stack10.push(i as i64);
-        assert_eq!(stack10.len(), i);
+        stack.push(i);
+        assert_eq!(stack.len(), i);
     }
 }
 
 #[test]
-fn stack10_pop_10() {
-    let mut stack10 = Stack10::new();
+fn stack_pop_10() {
+    let mut stack = Stack::new();
 
-    for i in 1..=10 { stack10.push(i); }
-
-    println!("{:?}", stack10);
+    for i in 1..=10 { stack.push(i); }
 
     for i in (1..=10).rev() {
-        assert_eq!(stack10.pop(), i)
+        assert_eq!(stack.pop(), Some(i));
     }
 }
